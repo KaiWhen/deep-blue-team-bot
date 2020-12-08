4868642a7ff9cc8f28ee8589e11458b11cbb25de
Kevin Wang <wangkw523@gmail.com>
2020-12-08 11:41:17 +0000

    oiauwbheguo

--- tweet.py
+++ tweet.py
@@ -25,10 +25,10 @@ split_text = f.split("\n====================\n")
 
 valid_text = [x for x in split_text if len(x) <= 280]
 
-for x in range(200):
+for x in range(2000):
     tweet = np.random.choice(valid_text, 1)
     api.update_status(tweet[0])
-    time.sleep(600)
+    time.sleep(14400)
 #for line in f:
  #   api.update_status(line)
   #  time.sleep(3600)
